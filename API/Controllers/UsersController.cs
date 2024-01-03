b8b809adb9f464b6dcb45ed7212bea913e5428ab
krishnaasx <mrpytonxr@gmail.com>
2024-01-03 16:29:11 +0530

    Second Commit (end of section 3)

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,29 +3,37 @@ using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Controllers;
-
-
-[ApiController]
-[Route("api/[controller]")] //api/users
-public class UsersController : ControllerBase
+namespace API.Controllers
 {
-    private readonly DataContext _context;
-    public UsersController(DataContext context)
+    [ApiController]
+    [Route("api/[controller]")] //api/users
+    public class UsersController : ControllerBase
     {
-        _context = context;
-    }
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
 
-    [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
-    {
-        var users = await _context.Users.ToListAsync();
-        return users;
-    }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return users;
+        }
 
-    [HttpGet("{id}")] //api/users/id
-    public async Task<ActionResult<AppUser>> GetUser(int id)
-    {
-        return await _context.Users.FindAsync(id);
+        [HttpGet("{id}")] //api/users/id
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return user;
+            }
+        }
     }
 }
